25f537b39ba578a79eccb1d9204843a66bb3d002
Alex Nisnevich <alex.nisnevich@gmail.com>
2013-03-08 20:32:47 -0800

    loading map from file now (exclamation mark)

--- game.js
+++ game.js
@@ -13,23 +13,35 @@ var LEFT = "left";
 var RIGHT = "right";
 
 var display;
+var map;
 var player;
 
 var dimensions = {
-	width: 80,
+	width: 50,
 	height: 25
 };
 
-var map = new Array(dimensions.width);
-for (var i = 0; i < dimensions.width; i++) {
-	map[i] = new Array(dimensions.height);
+var Map = function () {
+	// Initialize with empty grid
+	this._grid = new Array(dimensions.width);
+	for (var x = 0; x < dimensions.width; x++) {
+		this._grid[x] = new Array(dimensions.height);
+		for (var y = 0; y < dimensions.height; y++) {
+			this._grid[x][y] = 'empty';
+		}
+	}
+
+	this.placeObject = function (x, y, type) {
+		this._grid[x][y] = type;
+		display.draw(x, y, objects[type].symbol);
+	}
 }
 
 var objects = {
-    'empty' : {
-        'symbol': ' ',
-        'passable': true
-    },
+	'empty' : {
+		'symbol': ' ',
+		'passable': true
+	},
 
 	'block': {
 		'symbol': '#',
@@ -41,96 +53,80 @@ var objects = {
 	}
 };
 
-function init() {
-    display = new ROT.Display({width: dimensions.width, height: dimensions.height});
-    document.body.appendChild(display.getContainer());
-
-    display.setOptions( {fontSize: 20, fontStyle : "bold"});
-
-    // all maps start out initialized to completely empty squares
-    for (var x = 0; x < dimensions.width; x++) {
-        for (var y = 0; y < dimensions.height; y++) {
-            map[x][y] = 'empty';
-        }
-    }
-
-    startLevel1();
-}
-
 var Player = function(x,y) {
-    this._x = x;
-    this._y = y;
-    this._rep = "@";
-    this._fgColor = "#0f0";
-    this.draw();
+	this._x = x;
+	this._y = y;
+	this._rep = "@";
+	this._fgColor = "#0f0";
+	this.draw();
 }
 
 Player.prototype.draw = function () {
-    display.draw(this._x, this._y, this._rep, this._fgColor);
+	display.draw(this._x, this._y, this._rep, this._fgColor);
 }
 
 Player.prototype.move = function (direction) {
-    var cur_x = this._x;
-    var cur_y = this._y;
-    var new_x;
-    var new_y;
+	var cur_x = this._x;
+	var cur_y = this._y;
+	var new_x;
+	var new_y;
 
-    if (direction === UP) {
-        new_x = cur_x;
-        new_y = cur_y - 1;
-    }
-    else if (direction === DOWN) {
-        new_x = cur_x;
-        new_y = cur_y + 1;
-    }
-    else if (direction === LEFT) {
-        new_x = cur_x - 1;
-        new_y = cur_y;
-    }
-    else if (direction === RIGHT) {
-        new_x = cur_x + 1;
-        new_y = cur_y;
-    }
+	if (direction === UP) {
+		new_x = cur_x;
+		new_y = cur_y - 1;
+	}
+	else if (direction === DOWN) {
+		new_x = cur_x;
+		new_y = cur_y + 1;
+	}
+	else if (direction === LEFT) {
+		new_x = cur_x - 1;
+		new_y = cur_y;
+	}
+	else if (direction === RIGHT) {
+		new_x = cur_x + 1;
+		new_y = cur_y;
+	}
 
-    if (canMoveTo(new_x,new_y)) {
-        display.draw(cur_x,cur_y, objects[map[cur_x][cur_y]].symbol);
-        this._x = new_x;
-        this._y = new_y;
-        this.draw();
-    }
-    else {
-        console.log("Can't move to " + new_x + ", " + new_y + ", reported from inside Player.move() method");
-    }
+	if (canMoveTo(new_x,new_y)) {
+		display.draw(cur_x,cur_y, objects[map._grid[cur_x][cur_y]].symbol);
+		this._x = new_x;
+		this._y = new_y;
+		this.draw();
+	}
+	else {
+		console.log("Can't move to " + new_x + ", " + new_y + ", reported from inside Player.move() method");
+	}
 };
 
 function canMoveTo(x,y) {
-    return objects[map[x][y]].passable;
+	return objects[map._grid[x][y]].passable;
 }
 
-function placeObject(type, x, y) {
-	map[x][y] = type;
-	display.draw(x, y, objects[type].symbol);
+function init() {
+	display = new ROT.Display({width: dimensions.width, height: dimensions.height});
+	$('#screen').append(display.getContainer());
+
+	display.setOptions( {fontSize: 20, fontStyle : "bold"});
+
+	map = new Map();
+
+	$.get('levels/blocks', function (lvlCode) {
+		$('#editor').val(lvlCode);
+		evalLevelCode();
+	});
 }
 
-function startLevel1() {
-	for (y = 5; y <= dimensions.height - 5; y++) {
-    	placeObject('block', 5, y);
-    	placeObject('block', dimensions.width - 5, y);
-	}
-
-	for (x = 5; x <= dimensions.width - 5; x++) {
-    	placeObject('block', x, 5);
-    	placeObject('block', x, dimensions.height - 5);
-	}
-
-	player = new Player(15, 15);
+function evalLevelCode() {
+	eval($('#editor').val());
+	startLevel(map);
 }
 
 // Event listeners
 
 document.addEventListener("keydown", function(e) {
 	console.log(e.keyCode);
-    if (keys[e.keyCode]) {
-    	player.move(keys[e.keyCode]);
-    }
+	if (keys[e.keyCode]) {
+		map.player.move(keys[e.keyCode]);
+	}
 });
