4e173b7e115e306bd9a0077051077e2efc1c6f8b
Greg <elorian.mestec@gmail.com>
2013-03-08 23:33:37 -0800

    Made first level more flavorful

--- levels/blocks.js
+++ levels/blocks.js
@@ -1,5 +1,17 @@
-// {"editable": [[3, 11]]}
+// {"editable": [[14, 23]]}
+
+/*
+ * This is the first level, so we'll give you a hint.
+ * In fact, a really obvious hint (hey it's level 0).
+ * The code currently places blocks in a square surrounding
+ * you. All you need to do is make a gap.
+ * You don't even need to do anything extra. In fact,
+ * you should be doing less...
+ */
 function startLevel(map) {
+
+    map.player = new Player(15, 15);
+
     for (y = 5; y <= dimensions.height - 5; y++) {
         map.placeObject(5, y, 'block');
         map.placeObject(dimensions.width - 5, y, 'block');
@@ -10,8 +22,5 @@ function startLevel(map) {
         map.placeObject(x, dimensions.height - 5, 'block');
     }
 
-    map.player = new Player(15, 15);
-
-    // exit square always at bottom right corner
     map.placeObject(dimensions.width-1, dimensions.height-1, 'exit');
 }
