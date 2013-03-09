41f1f8932524d568fad661dd94a174dd108812ef
Greg <elorian.mestec@gmail.com>
2013-03-09 03:38:33 -0800

    Added reset level function

--- scripts/game.js
+++ scripts/game.js
@@ -182,6 +182,10 @@ function focusOnEditor() {
 	editor.focus();
 }
 
+function resetEditor() {
+    getLevel(currentLevel);
+}
+
 function evalLevelCode() {
 	var playerCode = editor.getValue();
 	var validatedStartLevel = validate(playerCode, currentLevel);
@@ -194,3 +198,4 @@ function evalLevelCode() {
 shortcut.add('ctrl+1', focusOnMap);
 shortcut.add('ctrl+2', focusOnEditor);
 shortcut.add('ctrl+5', evalLevelCode);
+shortcut.add('ctrl+4', resetEditor);
