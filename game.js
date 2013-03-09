e72d5e90fa81414c925dd2968cbbb8eb9106f48a
Greg <elorian.mestec@gmail.com>
2013-03-08 19:31:56 -0800

    Added player movement logic

--- game.js
+++ game.js
@@ -1,9 +1,15 @@
 
+// directions for moving entities
+var UP = "up";
+var DOWN = "down";
+var LEFT = "left";
+var RIGHT = "right";
+
 var display;
 var dimensions = {
 	width: 80,
 	height: 25
-}
+};
 
 var map = new Array(dimensions.width);
 for (var i = 0; i < dimensions.width; i++) {
@@ -11,6 +17,12 @@ for (var i = 0; i < dimensions.width; i++) {
 }
 
 var objects = {
+
+    'empty' : {
+        'symbol': ' ',
+        'passable': true
+    },
+    
 	'block': {
 		'symbol': '#',
 		'passable': false
@@ -25,8 +37,65 @@ function init() {
     display = new ROT.Display({width: dimensions.width, height: dimensions.height});
     document.body.appendChild(display.getContainer());
 
+    // all maps start out initialized to completely empty squares
+    for (var x = 0; x < dimensions.width; x++) {
+        for (var y = 0; y < dimensions.height; y++) {
+            map[x][y] = 'empty';
+        }
+    }
+
     startLevel1();
 }
+
+var Player = function(x,y) {
+    this._x = x;
+    this._y = y;
+    this._rep = "@";
+    this._fgColor = "#0f0";
+    this.draw();
+}
+
+Player.prototype.draw = function () {
+    display.draw(this._x, this._y, this._rep, this._fgColor);
+}
+
+Player.prototype.move = function (direction) {
+    var cur_x = this._x;
+    var cur_y = this._y;
+    var new_x;
+    var new_y;
+    if (direction === UP) {
+        new_x = cur_x;
+        new_y = cur_y - 1;
+    }
+    else if (direction === DOWN) {
+        new_x = cur_x;
+        new_y = cur_y + 1;
+    }
+    else if (direction === LEFT) {
+        new_x = cur_x + 1;
+        new_y = cur_y;
+    }
+    else if (direction === RIGHT) {
+        new_x = cur_x - 1;
+        new_y = cur_y;
+    }
+
+    if (canMoveTo(new_x,new_y)) {
+        display.draw(cur_x,cur_y, objects[map[cur_x][cur_y]].symbol);
+        this._x = new_x;
+        this._y = new_y;
+        this.draw();
+    }
+    else {
+        console.log("Can't move to " + new_x + ", " + new_y + ", reported from inside Player.move() method");
+    }
+};
+
+function canMoveTo(x,y) { //TODO flesh this out
+    return true;
+}
+
 
 function placeObject(type, x, y) {
 	map[x][y] = type;
