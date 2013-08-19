bd91b7c0f2b6752a11183651c3906e242f4d43b7
Alex Nisnevich <alex.nisnevich@gmail.com>
2013-08-19 00:42:55 -0700

    fixing traps level

--- levels/traps.jsx
+++ levels/traps.jsx
@@ -23,16 +23,16 @@ function startLevel(map) {
 
     map.placePlayer(map.getWidth() - 5, 5);
 
-    for (var i = 0; i < 50; i++) {
+    for (var i = 0; i < 75; i++) {
         var x = getRandomInt(0, map.getWidth() - 1);
         var y = getRandomInt(0, map.getHeight() - 1);
-        if (x != map.getWidth() - 1 && y != map.getHeight() - 1) {
+        if (x != 2 || y != map.getHeight() - 1) {
             // don't place trap over exit!
             map.placeObject(x, y, 'trap', '#f00');
-        }
 #BEGIN_EDITABLE#
 
 #END_EDITABLE#
+        }
     }
 
     map.placeObject(2, map.getHeight() - 1, 'exit');
