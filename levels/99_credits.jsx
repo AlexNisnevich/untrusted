60067b4eb4d6d4bd153d16871eb84dd398559424
Alex Nisnevich <alex.nisnevich@gmail.com>
2013-12-23 19:49:24 -0800

    up-to-date credits

--- levels/99_credits.jsx
+++ levels/99_credits.jsx
@@ -4,20 +4,20 @@
 }
 #END_PROPERTIES#
 function startLevel(map) {
-    // TODO: music not up-to-date
     var credits = [
-    	[15, 1, "U N T R U S T E D"],
-    	[20, 2, "- or -"],
-    	[5, 3, "THE CONTINUING ADVENTURES OF DR. EVAL"],
-    	[1, 5, "a game by Alex Nisnevich and Greg Shuflin"],
-    	[1, 8, "/* MUSIC */"],
-    	[1, 10, "Game Scratch - Dmitry Mazin (w/ permission)"],
-    	[1, 11, "Zero_One - Jackson D. (CC-BY-NC-ND)"],
-    	[1, 12, "intricate - cloudy sin (CC-BY-NC-ND)"],
-    	[1, 13, "System - Skizofonik (CC-BY-NC-ND)"],
-    	[1, 14, "Sunset 84 - Spectrofuzz (CC-BY-ND)"],
-        [1, 15, "Beach Wedding Dance - Rolemusic (CC-BY-NC-SA)"],
-    	[1, 16, "The Green - Yonnie (CC-BY-ND)"],
+    	[15, 2, "U N T R U S T E D"],
+    	[20, 3, "- or -"],
+    	[5, 4, "THE CONTINUING ADVENTURES OF DR. EVAL"],
+    	[1, 6, "a game by Alex Nisnevich and Greg Shuflin"],
+    	[1, 9, "/* MUSIC */"],
+    	[1, 11, "Game Scratch - Dmitry Mazin"],
+    	[1, 12, "Y - Tortue Super Sonic"],
+    	[1, 13, "Come and Find Me - Eric Skiff"],
+    	[1, 14, "Searching - Eric Skiff"],
+        [1, 15, "The Waves Call Her Name - Sycamore Drive"],
+        [1, 16, "Slimeball Vomit - Radio Scotvoid"],
+        [1, 17, "Beach Wedding Dance - Rolemusic"],
+    	[1, 18, "Brazilian Rhythm - Vernon Lenoir"],
     	[15, map.getHeight() - 2, "Thank you for playing!"]
     ];
 
