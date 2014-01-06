3b730aa215ef6c8bb2927391df956b8f6525674c
Alex Nisnevich <alex.nisnevich@gmail.com>
2014-01-06 04:22:23 -0800

    Wrapping all JS into a closure
    Fixes #29.

--- scripts/_launcher_debug.js
+++ scripts/_launcher_debug.js
@@ -1,7 +1,6 @@
-var game;
 $(document).ready(function() {
 	var startLevel = getParameterByName('lvl') ? parseInt(getParameterByName('lvl')) : null;
-	game = new Game(true, startLevel);
-	game._initialize();
+	window.game = new Game(true, startLevel);
+	window.game._initialize();
 	window.eval = {};
 });
