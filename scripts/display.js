7469bbd77883a7b139a8035fdcf3cce3c83d579d
Alex Nisnevich <alex.nisnevich@gmail.com>
2014-02-10 03:54:41 -0800

    super-sketchy display.renderDom() method

--- scripts/display.js
+++ scripts/display.js
@@ -225,3 +225,22 @@ ROT.Display.prototype.focus = function() {
     $('#screen').show();
     $(this.getContainer()).attr('tabindex', '0').click().focus();
 };
+
+// using ideas from http://robert.ocallahan.org/2011/11/drawing-dom-content-to-canvas.html
+ROT.Display.prototype.renderDom = function(html) {
+    var ctx = $('#drawingCanvas')[0].getContext("2d");
+    var data = "data:image/svg+xml," +
+               "<svg xmlns='http://www.w3.org/2000/svg' width='200' height='200'>" +
+                 "<foreignObject width='100%' height='100%'>" +
+                   "<div xmlns='http://www.w3.org/1999/xhtml'>" +
+                        html +
+                   "</div>" +
+                 "</foreignObject>" +
+               "</svg>";
+    console.log(data);
+    var img = new Image();
+    img.src = data;
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+    }
+}
