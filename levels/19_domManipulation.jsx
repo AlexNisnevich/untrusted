7b671cb2e5bf5c90d9bcd4bb7c7063f3ee6a370c
greg <elorian.mestec@gmail.com>
2014-03-23 18:43:58 -0700

    Fleshing out DOM level page
    We need some complicated DOM to make the DOM level interesting. Surely
    more still could be added but this is a start.

--- levels/19_domManipulation.jsx
+++ levels/19_domManipulation.jsx
@@ -23,7 +23,7 @@
 function startLevel(map) {
     map.placePlayer(1, 1);
 
-    var html = "<div class='offLimits'>" +
+    var html = "<div class='pageContainer'>" +
     "<div style='width: 600px; height: 500px; background-color: white; font-size: 10px;'>" +
         "<center><h1>Department of Theoretical Computation</h1></center>" +
         "<hr />" +
@@ -31,15 +31,42 @@ function startLevel(map) {
             "<td><div id='face' /></td>" +
             "<td>" +
                 "<h2>Cornelius Eval</h2>" +
-                "<h3>Professor</h3>" +
-                "Fields of study:" +
+                "<h3>Associate Professor of Computer Science</h3>" +
                 "<ul>" +
-                    "<li>Human-computer interface</li>" +
-                    "<li>NP completeness</li>" +
+                    "<li>BS, Mathematics, University of Manitoba</li>" +
+                    "<li>PhD, Theoretical Computation, <a href='http://www.mit.edu'>MIT</a></li>" +
                 "</ul>" +
             "</td>" +
         "</tr></table>" +
-        "<hr />" +
+        "<h4>About me</h4>" +
+        "<p>I am an associate professor of computer science, attached to the Department of " +
+        "Theoretical Computation. My current research interests include the human-machine " +
+        "interface, NP complete problems, and parallelized mesh mathematics.</p>" +
+        "<p>I am also the current faculty advisor to the <a href=''>undergraduate Super Smash Bros. team</a>. " +
+        "In my spare time I enjoy polka and dirtbiking. </p>" +
+
+        "<div id='class_schedule'>" +
+          "<h4>Class Schedule</h4>" +
+            "<table>" +
+             "<tr>" +
+                "<th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th>" +
+             "</tr>" +
+             "<tr>" +
+                "<td>CS145 - Semicolons</td><td>Nothing Planned</td><td>CS145 - Semicolons</td><td>CS199 - Practical Theorycrafting </td><td>CS145 - Semicolons</td>" +
+             "</tr>" +
+            "</table>" +
+        "</div>" +
+        "<div id='loremIpsum'>" +
+        "<h4>Lorem Ipsum</h4>" +
+          "<blockquote>" +
+            "<code>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci " +
+            "velit, sed quia nonnumquam eiusmodi tempora incidunt ut labore et dolore magnam aliquam quaerat " +
+            "voluptatem.</code>" +
+            "<footer>— " +
+              "<cite>Cicero, De Finibus Bonorum et Malorum</cite>" +
+            "</footer>" +
+          "</blockquote>" +
+        "</div>" +
     "</div></div>";
 
     var $dom = $(html);
@@ -50,7 +77,7 @@ function startLevel(map) {
     map.overrideKey('up', function () {
         var currentPosition = $dom.find('.drEvalLocation');
         if (currentPosition.parent().length > 0 &&
-                !currentPosition.parent().hasClass('offLimits')) {
+                !currentPosition.parent().hasClass('pageContainer')) {
             currentPosition.parent().addClass('drEvalLocation');
             currentPosition.removeClass('drEvalLocation');
             map.updateDOM($dom);
