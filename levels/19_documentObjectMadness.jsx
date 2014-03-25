9ba455e98e87271b0d4cbf8e028d005d8e6d1c77
Alex Nisnevich <alex.nisnevich@gmail.com>
2014-03-24 19:04:40 -0700

    fixing merge

--- levels/19_documentObjectMadness.jsx
+++ levels/19_documentObjectMadness.jsx
@@ -1,6 +1,6 @@
 #BEGIN_PROPERTIES#
 {
-
+    "music": "BossLoop"
 }
 #END_PROPERTIES#
 /**********************
@@ -20,9 +20,11 @@
  *
  */
 
-function startLevel(map) {
-    map.placePlayer(1, 1);
+function objective(map) {
+    return map.getDOM().find('.adversary').hasClass('drEval');
+}
 
+function startLevel(map) {
     var html = "<div class='pageContainer'>" +
     "<div style='width: 600px; height: 500px; background-color: white; font-size: 10px;'>" +
         "<center><h1>Department of Theoretical Computation</h1></center>" +
@@ -71,49 +73,71 @@ function startLevel(map) {
 
     var $dom = $(html);
 
-    $dom.find('ul').addClass('drEvalLocation');
-    $dom.find('h2').addClass('adversaryLocation');
+    $dom.find('ul').addClass('drEval');
+    $dom.find('h2').addClass('adversary');
 
-    map.overrideKey('up', function () {
-        var currentPosition = $dom.find('.drEvalLocation');
+    function moveToParent(className) {
+        var currentPosition = $dom.find('.' + className);
         if (currentPosition.parent().length > 0 &&
-                !currentPosition.parent().hasClass('pageContainer')) {
-            currentPosition.parent().addClass('drEvalLocation');
-            currentPosition.removeClass('drEvalLocation');
+                !currentPosition.parent().hasClass('container')) {
+            currentPosition.parent().addClass(className);
+            currentPosition.removeClass(className);
             map.updateDOM($dom);
-            console.log(currentPosition);
         }
-    });
+    }
 
-    map.overrideKey('down', function () {
-        var currentPosition = $dom.find('.drEvalLocation');
+    function moveToFirstChild(className) {
+        var currentPosition = $dom.find('.' + className);
         if (currentPosition.children().length > 0) {
-            currentPosition.children().first().addClass('drEvalLocation');
-            currentPosition.removeClass('drEvalLocation');
+            currentPosition.children().first().addClass(className);
+            currentPosition.removeClass(className);
             map.updateDOM($dom);
-            console.log(currentPosition);
         }
-    });
+    }
 
-    map.overrideKey('left', function () {
-        var currentPosition = $dom.find('.drEvalLocation');
+    function moveToPreviousSibling(className) {
+        var currentPosition = $dom.find('.' + className);
         if (currentPosition.prev().length > 0) {
-            currentPosition.prev().first().addClass('drEvalLocation');
-            currentPosition.removeClass('drEvalLocation');
+            currentPosition.prev().first().addClass(className);
+            currentPosition.removeClass(className);
             map.updateDOM($dom);
-            console.log(currentPosition);
+        }
+    }
+
+    function moveToNextSibling(className) {
+        var currentPosition = $dom.find('.' + className);
+        if (currentPosition.next().length > 0) {
+            currentPosition.next().first().addClass(className);
+            currentPosition.removeClass(className);
+            map.updateDOM($dom);
+        }
+    }
+
+    map.overrideKey('up', function () { moveToParent('drEval'); });
+    map.overrideKey('down', function () { moveToFirstChild('drEval'); });
+    map.overrideKey('left', function () { moveToPreviousSibling('drEval'); });
+    map.overrideKey('right', function () { moveToNextSibling('drEval'); });
+
+    map.defineObject('adversary', {
+        'type': 'dynamic',
+        'symbol': '@',
+        'color': 'red',
+        'behavior': function (me) {
+            var move = Math.floor(Math.random() * 4) + 1; // 1, 2, 3, or 4
+            if (move == 1) {
+                moveToParent('adversary');
+            } else if (move == 2) {
+                moveToFirstChild('adversary');
+            } else if (move == 3) {
+                moveToPreviousSibling('adversary');
+            } else if (move == 4) {
+                moveToNextSibling('adversary');
+            }
         }
     });
 
-    map.overrideKey('right', function () {
-        var currentPosition = $dom.find('.drEvalLocation');
-        if (currentPosition.next().length > 0) {
-            currentPosition.next().first().addClass('drEvalLocation');
-            currentPosition.removeClass('drEvalLocation');
-            map.updateDOM($dom);
-            console.log(currentPosition);
-        }
-    });
+    map.placePlayer(1, 1);
+    map.placeObject(map.getWidth() - 2, map.getHeight() - 2, 'adversary');
 
     map.createFromDOM($dom);
 #END_OF_START_LEVEL#
