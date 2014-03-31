f8f5eb3140ca43b51a834ed979b4d77fc7c16161
Alex Nisnevich <alex.nisnevich@gmail.com>
2014-03-30 23:34:58 -0700

    preventing ctrl+R and F5

--- scripts/_launcher_release.js
+++ scripts/_launcher_release.js
@@ -2,3 +2,13 @@ $(document).ready(function() {
     new Game()._initialize();
     window.eval = {};
 });
+
+// prevent ctrl+R and F5
+$(document).bind('keydown keyup', function(e) {
+    if(e.which === 116) {
+       return false;
+    }
+    if(e.which === 82 && e.ctrlKey) {
+       return false;
+    }
+});
