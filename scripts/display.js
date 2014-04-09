1ab3af5577a9291f76379227b1b020cf5eba9c83
Alex Nisnevich <alex.nisnevich@gmail.com>
2014-04-09 16:31:09 -0700

    a game by Alex Nisnevich and Greg Shuflin

--- scripts/display.js
+++ scripts/display.js
@@ -159,7 +159,8 @@ ROT.Display.prototype.playIntro = function (map, i) {
         this.drawText(15, i + 3, "U N T R U S T E D");
         this.drawText(20, i + 5, "- or - ");
         this.drawText(5, i + 7, "THE CONTINUING ADVENTURES OF DR. EVAL");
-        this.drawText(10, i + 20, "Press any key to begin ...")
+        this.drawText(3, i + 12, "a game by Alex Nisnevich and Greg Shuflin");
+        this.drawText(10, i + 22, "Press any key to begin ...")
         setTimeout(function () {
             display.playIntro(map, i - 1);
         }, 100);
