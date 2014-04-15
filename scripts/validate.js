5202ab98eae86080650b706752b90c353295edd8
phoeagon <phoeagon@gmail.com>
2014-04-15 12:34:58 +0800

    prevent bypassing validation using base64

--- scripts/validate.js
+++ scripts/validate.js
@@ -10,6 +10,7 @@ Game.prototype.verbotenWords = [
     'prompt', 'confirm', // prevents dialogs asking for user input
     'debugger', // prevents pausing execution
     'delete', // prevents removing items
+    'atob','btoa',//prevent bypassing checks using Base64
     'window', // prevents setting "window.[...] = map", etc.
     'document', // in particular, document.write is dangerous
     'self.', 'self[', 'top.', 'top[', 'frames',  // self === top === frames === window
