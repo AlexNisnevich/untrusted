b4792cd0f9c5c11c652054a7cf54d937c3169ae2
Xing Xing <mikespook@gmail.com>
2014-10-10 11:31:38 +0800

    mods will have their own intro

--- parse_target.sh
+++ parse_target.sh
@@ -2,24 +2,23 @@
 target=$1
 mod=$2
 
+# MOD
 sed -i "s#\/\/%MOD%#$mod#" $target
 
 [ -z $mod ] && mod=default
 
+# LEVELS
 levels=''
-
 for lvl in mods/$mod/*.jsx
 do
 	lvl=`basename $lvl`
 	levels=`printf "$levels'%s'," $lvl`
 done
-
 levels="${levels%?}"
-
 sed -i "s#\/\/%LEVELS%#$levels#g" $target
 
+# BONUS
 bonus=''
-
 if [ -d mods/$mod/bonus ]; then
 	for lvl in mods/$mod/bonus/*.jsx
 	do
@@ -27,7 +26,15 @@ if [ -d mods/$mod/bonus ]; then
 		bonus=`printf "$bonus'%s'," $lvl`
 	done
 fi
-
 bonus="${bonus%?}"
-
 sed -i "s#\/\/%BONUS%#$bonus#" $target
+
+# INTRO
+f=mods/default/intro.jsi
+[ -f mods/$mod/intro.jsi ] && f=mods/$mod/intro.jsi
+IFS="
+"
+for l in `cat $f`; do
+	l=`echo $l|sed "s|#|\\\\\#|"`
+	sed -i "s#\/\/%INTRO%#$l\n\/\/%INTRO%#" $target
+done
