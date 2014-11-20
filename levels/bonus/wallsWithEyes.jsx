5d08ab5f9793a23665c99a51e2226942e4d2ad44
ishanv <ishanvirk@gmail.com>
2014-11-20 15:53:55 -0800

    Updated level to have start-up map completed, algorithm still needed

--- levels/bonus/wallsWithEyes.jsx
+++ levels/bonus/wallsWithEyes.jsx
@@ -22,5 +22,14 @@ function startLevel(map) {
 #START_OF_START_LEVEL#
     map.placePlayer(map.getWidth()/2, 1);
     map.placeObject(map.getWidth()/2, map.getHeight()-1, 'exit');
+	//Should place initial walls from top to bottom of thickness 5
+        for (var i = 0; i < map.getHeight(); i++) {
+			for(var j = 0 j <= 5; j++){
+			map.placeObject(j, i, 'block');
+			}
+			for(var k = getWidth()-5 j <= getWidth(); k++){
+			map.placeObject(k, i, 'block');
+			}
+		}         
 #END_OF_START_LEVEL#
 }
