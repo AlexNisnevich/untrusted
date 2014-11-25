6859d2068e398c1103a45489727723993a7bf614
Jianyu Liu <liujianyu@hotmail.com>
2014-11-24 16:17:15 -0800

    Added the level to the bonus directory.

--- levels/bonus/wallsWithEyes.jsx
+++ levels/bonus/wallsWithEyes.jsx
@@ -1,7 +1,10 @@
 #BEGIN_PROPERTIES#
 {
     "version": "1.2.1",
-    "commandsIntroduced": ['object.pushable'],
+    "commandsIntroduced":
+        ["global.startLevel", "global.onExit", "map.placePlayer",
+         "map.placeObject", "map.getHeight", "map.getWidth",
+         "map.displayChapter", "map.getPlayer", "player.hasItem"],
     "music": "Brazil"
 }
 #END_PROPERTIES#
@@ -15,19 +18,20 @@
  * Ya...that was Indiana Jones
  * Good luck picking up the item...just remember the walls have eyes...
  * ...If Indiana Jones couldn't get there, what makes you think you will...
+ * Commands added
  */
  
 
 function startLevel(map) {
 #START_OF_START_LEVEL#
-    map.placePlayer(map.getWidth()/2, 1);
-    map.placeObject(map.getWidth()/2, map.getHeight()-1, 'exit');
+    map.placePlayer(map.getWidth()/ 2, 1);
+    map.placeObject(map.getWidth()/ 2, map.getHeight()-1, 'exit');
 	//Should place initial walls from top to bottom of thickness 5
         for (var i = 0; i < map.getHeight(); i++) {
-			for(var j = 0 j <= 5; j++){
+			for(var j = 0; j <= 5; j++){
 			map.placeObject(j, i, 'block');
 			}
-			for(var k = getWidth()-5; j <= getWidth(); k++){
+			for(var k = map.getWidth()-5; k<= map.getWidth(); k++){
 			map.placeObject(k, i, 'block');
 			}
 		}         
