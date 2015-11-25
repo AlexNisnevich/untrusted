6384127d6bd247765de24042decd5376dae4c239
phelan <phelan@live.ca>
2015-11-24 19:40:07 -0800

    completed moving ball functionality for issue #6 and door placement for issue #4

--- levels/bonus/soccer.jsx
+++ levels/bonus/soccer.jsx
@@ -1,7 +1,10 @@
 #BEGIN_PROPERTIES#
 {
-
-
+	"version": "1.0"
+	"mapProperties": {
+		"refreshRate": 50,
+		//"quickValidateCallback": true // <-- not sure what this does or if needed
+	}
 }
 #END_PROPERTIES#
 /*****************************
@@ -15,6 +18,8 @@ function startLevel(map){
 #START_OF_START_LEVEL#
 // Create map here (or after object definitions)
 
+var kickedDirection = 'none';
+var kickedDirection = 0;
 
 
 map.defineObject('enemyPlayer', {
@@ -27,6 +32,44 @@ map.defineObject('goalie', {
 
 map.defineObject('ball', {
 	// Define ball here
+	'type': 'dynamic',
+	'symbol': 'o',
+	'behaviour': function (me) {
+		if (kickedDirection != 'none' && kickedDistance > 0){
+			if (me.canMove(kickedDirection)){
+				me.move(kickedDirection);
+				kickedDistance--;
+			}
+			else{
+				kickedDistance = 0;
+			}
+		}
+		if (kickedDirection != 'none' && kickedDistance > 0 && me.canMove(kickedDirection))
+		if (me.getX() == (map.getWidth - 1) && me.getY() < 15 && me.getY() > 10){ // <-- change to actual goal post locations
+			map.placeObject(4, map.getHeight() - 4, 'exit');
+		}
+	}
+});
+
+map.getPlayer().setPhoneCallback(function () {
+	var x = map.getPlayer().getX();
+	var y = map.getPlayer().getY();
+	if (map.getObjectTypeAt(x + 1, y) == 'ball'){
+		kickedDirection = 'right';
+		kickedDistance = 10;
+	}
+	else if (map.getObjectTypeAt(x - 1, y) == 'ball'){
+		kickedDirection = 'left';
+		kickedDistance = 10;
+	}
+	else if (map.getObjectTypeAt(x, y + 1) == 'ball'){
+		kickedDirection = 'up';
+		kickedDistance = 10;
+	}
+	else if (map.getObjectTypeAt(x, y - 1) == 'ball'){
+		kickedDirection = 'down';
+		kickedDistance = 10;
+	}
 });
 
 // More stuff probably goes here
