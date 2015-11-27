f1be343e6ff64f23e4fba2b69cef127355b5eb84
alexsclim <alexsclim@gmail.com>
2015-11-26 18:12:42 -0800

    added invisible walls and teleportation

--- levels/bonus/bonus.jsx
+++ levels/bonus/bonus.jsx
@@ -1,7 +1,7 @@
 #BEGIN_PROPERTIES#
 {
     "version": "1.2.1",
-    "commandsIntroduced": ['map.startTimer'],
+    "commandsIntroduced": ['map.startTimer', "object.setTarget", "map.getDynamicObjects"],
     "music": "Brazil"
 }
 #END_PROPERTIES#
@@ -10,6 +10,11 @@
 function startLevel(map) {
 #START_OF_START_LEVEL#
  var savedX, savedY, savedDirection;
+    function shuffle(o){ //v1.0 [http://bit.ly/1l6LGQT]
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i),
+            x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    };
     map.defineObject('ice', {
       'symbol': String.fromCharCode(0x2630), 'color': '#75D1FF',
       'impassable': function(player, me) {
@@ -66,7 +71,7 @@ function startLevel(map) {
     'impassable' : true
   });
     map.createFromGrid([
-'                           #                      ',
+'o                          #                      ',
 '                           #                      ',
 '                           #                E     ',
 '                           #                      ',
@@ -84,7 +89,7 @@ function startLevel(map) {
 'xx#                        #                      ',
 'xx#                        #                      ',
 'xx#                        #                      ',
-'xx#                                               ',
+'xx#                        o                      ',
 'xx#                        #                      ',
 'xx#                        #                      ',
 'xx#                        #                      ',
@@ -98,8 +103,9 @@ function startLevel(map) {
         'i': 'invisibleBoulder',
         'x': 'ice',
         'C': 'computer',
+        'o': 'teleporter'
     }, 0, 0);
-  function gravity() {
+  function gravity(trigger) {
     var player = map.getPlayer();
     var x = player.getX();
     var y = player.getY() + 1;
@@ -114,10 +120,59 @@ function startLevel(map) {
   }
   map.startTimer(gravity,45);
 
-  map.placeObject(0, 0, 'phone');
-  map.getPlayer().setPhoneCallback(function () {
+  var teleporters = map.getDynamicObjects();
+  teleporters = shuffle(teleporters);
+  var t1 = teleporters[0];
+  var t2 = teleporters[1];
+  t1.setTarget(t2);
+  t2.setTarget(t1);
 
-  });
+  for(x = 3; x < 26; x++) {
+    map.placeObject(x, 10, 'invisibleBoulder');
+  }
+  for(x = 4; x < 27; x++) {
+    map.placeObject(x, 8, 'invisibleBoulder');
+  }
+  for(x = 0; x < 26; x++) {
+    map.placeObject(x, 6, 'invisibleBoulder');
+  }
+  for(y = 1; y < 6; y++) {
+    map.placeObject(25, y, 'invisibleBoulder');
+  }
+  for(y = 0; y < 5; y++) {
+    map.placeObject(23, y, 'invisibleBoulder');
+  }
+  for(y = 1; y < 6; y++) {
+    map.placeObject(21, y, 'invisibleBoulder');
+  }
+  for(y = 0; y < 5; y++) {
+    map.placeObject(19, y, 'invisibleBoulder');
+  }
+  for(y = 1; y < 6; y++) {
+    map.placeObject(17, y, 'invisibleBoulder');
+  }
+  for(y = 0; y < 5; y++) {
+    map.placeObject(15, y, 'invisibleBoulder');
+  }
+  for(y = 1; y < 6; y++) {
+    map.placeObject(13, y, 'invisibleBoulder');
+  }
+  for(y = 0; y < 5; y++) {
+    map.placeObject(11, y, 'invisibleBoulder');
+  }
+  for(y = 1; y < 6; y++) {
+    map.placeObject(9, y, 'invisibleBoulder');
+  }
+  for(y = 0; y < 5; y++) {
+    map.placeObject(7, y, 'invisibleBoulder');
+  }
+  for(y = 1; y < 6; y++) {
+    map.placeObject(5, y, 'invisibleBoulder');
+  }
+  for(y = 0; y < 5; y++) {
+    map.placeObject(3, y, 'invisibleBoulder');
+  }
+
 #BEGIN_EDITABLE#
   for(x = 43; x < 46; x++) {
     map.placeObject(x, 1, 'boulder');
@@ -126,12 +181,6 @@ function startLevel(map) {
   map.placeObject(43, 2, 'boulder');
   map.placeObject(45, 2, 'boulder');
   map.placeObject(1, 9, 'boulder');
-  for(x = 3; x < 26; x++) {
-    map.placeObject(x, 10, 'invisibleBoulder');
-  }
-  for(x = 4; x < 27; x++) {
-    map.placeObject(x, 8, 'invisibleBoulder');
-  }
 #END_EDITABLE#
 #END_OF_START_LEVEL#
 }
