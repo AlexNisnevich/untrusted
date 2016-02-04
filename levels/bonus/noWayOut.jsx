a8640391a7fe2d41edb7e218481aa8122fa24d35
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-02-04 13:34:46 -0800

    fix noWayOut.jsx

--- levels/bonus/noWayOut.jsx
+++ levels/bonus/noWayOut.jsx
@@ -1,11 +1,11 @@
-/*#BEGIN_PROPERTIES#*/
+#BEGIN_PROPERTIES#
 {
     "version": "0.1000002",
     "commandsIntroduced":
     [ ],
         "music": "gurh"
 }
-/*#END_PROPERTIES#*/
+#END_PROPERTIES#
 /*******************
  * 102_noWayOut.js *
  * by AGoliath     *
@@ -16,7 +16,7 @@
  */
 
 function startLevel(map) {
-/*#START_OF_START_LEVEL#*/
+#START_OF_START_LEVEL#
     map.displayChapter('Challenge 2\nTear down this wall!');
 
     map.placePlayer(7, 5);
@@ -25,13 +25,13 @@ function startLevel(map) {
         for (var i= 0; i <= map.getHeight(); i++)  map.placeObject(map.getWidth()-10, i, 'block');
     }
 
-/*#BEGIN_EDITABLE#*/
+#BEGIN_EDITABLE#
 
-/*#END_EDITABLE#*/
+#END_EDITABLE#
 
         buildWall();
         map.placeObject(map.getWidth()-7, map.getHeight()-5, 'exit');
-/*#END_OF_START_LEVEL#*/
+#END_OF_START_LEVEL#
 }
 
 function onExit(map) {
