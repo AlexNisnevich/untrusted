3b422362b6970620184cf8c50e296651197021fb
PC-UTILISATEUR\Utilisateur <1456204@cstjean.qc.ca>
2018-03-05 22:01:51 -0500

    no message

--- mods/default/intro.js
+++ mods/default/intro.js
@@ -7,7 +7,7 @@ function playIntro(display, map, i) {
         display.drawText(0, i - 2, "%c{#0f0}> initialize");
         display.drawText(13, i + 3, "R I S E O F T H E M O D S");
         display.drawText(22, i + 5, "- or - ");
-        display.drawText(16, i + 7, "THE EXAMPLE OF MODS");
+        display.drawText(16, i + 7, "THE EXAMPLE OF Minou");
         display.drawText(5, i + 12, "a demo that shows how to develop a mod");
         display.drawText(10, i + 22, "Press any key to begin ...");
         setTimeout(function () {
