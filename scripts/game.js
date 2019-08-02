AlexNisnevich/untrusted

commit 0050642797e76d5ab5a3a1bdb409a17720e18ce6

Validate timer callbacks (#433)
Closes #406

scripts/map.js
@@ -517,8 +517,8 @@ function Map(display, __game) {
         } else if (delay < 25) {
             throw "startTimer(): minimum delay is 25 milliseconds";
         }
-
-        __intervals.push(setInterval(timer, delay));
+        var validate = this._validateCallback;
+        __intervals.push(setInterval(function(){validate(timer)}, delay));
     }, this);
 
     this.timeout = wrapExposedMethod(function(timer, delay) {
@@ -527,8 +527,8 @@ function Map(display, __game) {
         } else if (delay < 25) {
             throw "timeout(): minimum delay is 25 milliseconds";
         }
-
-        __intervals.push(setTimeout(timer, delay));
+        var validate = this._validateCallback;
+        __intervals.push(setTimeout(function(){validate(timer)}, delay));
     }, this);
 
     this.displayChapter = wrapExposedMethod(function(chapterName, cssClass) {

commit 363c9a4b6873574745b81d5d6b9817ee05086359

Better handling of status messages
Fixes a bunch of bugs related to the handling of `map.writeStatus`:

    1. In levels with timers, but without a `refreshRate` (ex: level 18, the lamrynth bonus level), status messages disappear whenever the timer next runs, usdually too soon to read them (this bug was introduced by #433)
    2. In levels with a `refreshRate` (ex: level 20), certain kinds of status message (the "are you sure you want to reset the level" message, the error message when trying to use the phone with no phone callback) disappear almost immiedately
    3. Multiple status messages can be written on top of each other.
    4. The "do you want to reset the game" status message doesn't disappear when the reset timer expires.

scripts/game.js
@@ -276,6 +276,7 @@ function Game(debugMode, startLevel) {
     this._resetLevel = function( level ) {
         var game = this;
         var resetTimeout_msec = 2500;
+        var reset_game_msg = "To reset this level press ^4 again.";
 
         if ( this._resetTimeout != null ) {
             $('body, #buttons').css('background-color', '#000');
@@ -287,12 +288,18 @@ function Game(debugMode, startLevel) {
             } else {
                 this._getLevel(level, true);
             }
+            if(game.map._status == reset_game_msg) {
+                game.map.writeStatus("");
+            }
         } else {
-            this.display.writeStatus("To reset this level press ^4 again.");
+            this.map.writeStatus(reset_game_msg);
             $('body, #buttons').css('background-color', '#900');
 
             this._resetTimeout = setTimeout(function () {
                 game._resetTimeout = null;
+                if(game.map._status == reset_game_msg) {
+                    game.map.writeStatus("");
+                }
 
                 $('body, #buttons').css('background-color', '#000');
             }, resetTimeout_msec );
@@ -374,7 +381,7 @@ function Game(debugMode, startLevel) {
                 // workaround because we can't use writeStatus() in startLevel()
                 // (due to the text getting overwritten by the fade-in)
                 if (game.editor.getProperties().startingMessage) {
-                    game.display.writeStatus(game.editor.getProperties().startingMessage);
+                    game.map.writeStatus(game.editor.getProperties().startingMessage);
                 }
             });
 

scripts/inventory.js
@@ -80,7 +80,7 @@ Game.prototype.setInventoryStateByLevel = function (levelNum) {
 	}
 
     // clear any status messages generated by this
-    this.map._status = '';
+    this.map.writeStatus("");
 };
 
 Game.prototype.drawInventory = function () {
@@ -113,7 +113,7 @@ Game.prototype.usePhone = function () {
 			this.validateCallback(player._phoneFunc);
 		} else {
 			this.sound.playSound('static');
-			this.display.writeStatus("Your function phone isn't bound to any function!");
+			this.map.writeStatus("Your function phone isn't bound to any function!");
 		}
 	}
 };

scripts/map.js
@@ -114,26 +114,19 @@ function Map(display, __game) {
     this._ready = function () {
         if (__game._isPlayerCodeRunning()) { throw 'Forbidden method call: map._ready()';}
 
-        var map = this;
-
         // set refresh rate if one is specified
         if (__refreshRate) {
-            // wrapExposedMethod is necessary here to prevent the game from thinking
-            //  `map._status` is an unauthorized attempt to access a private attribute.
-            map.startTimer(wrapExposedMethod(function () {
+            // wrapExposedMethod is necessary here to prevent the `_moveToNextLevel`
+            // call from breaking
+            this.startTimer(wrapExposedMethod(function () {
                 // refresh the map
-                map.refresh();
-
-                // rewrite status
-                if (map._status) {
-                    map.writeStatus(map._status);
-                }
+                this.refresh();
 
                 // check for nonstandard victory condition
                 if (typeof(__game.objective) === 'function' && __game.objective(map)) {
                     __game._moveToNextLevel();
                 }
-            }), __refreshRate);
+            }, this), __refreshRate);
         }
     };
 
@@ -182,13 +175,9 @@ function Map(display, __game) {
                 return true;
             } else if (typeof object.impassable === 'function') {
                 // the obstacle is impassable only in certain circumstances
-                try {
-                    return this._validateCallback(function () {
-                        return !object.impassable(__player, object);
-                    });
-                } catch (e) {
-                    display.writeStatus(e.toString());
-                }
+                return this._validateCallback(function () {
+                    return !object.impassable(__player, object);
+                });
             } else {
                 // the obstacle is always impassable
                 return false;
@@ -330,9 +319,6 @@ function Map(display, __game) {
         __chapterHideTimeout = setTimeout(function () {
             $('#chapter').fadeOut(1000);
         }, $('#chapter').hasClass('death') ? 2500 : 0);
-
-        // also, clear any status text if map is refreshing automatically (e.g. boss level)
-        this._status = '';
     };
 
     this._refreshDynamicObjects = function() {
@@ -383,6 +369,10 @@ function Map(display, __game) {
             this._display.renderDom(domHTML, __domCSS);
         } else {
             this._display.drawAll(this);
+        }
+        // rewrite any status messages
+        if (this._status) {
+            this._display.writeStatus(this._status);
         }
         __game.drawInventory();
     }, this);
@@ -561,17 +551,13 @@ function Map(display, __game) {
     }, this);
 
     this.writeStatus = wrapExposedMethod(function(status) {
-        this._status = status;
-
-        if (__refreshRate) {
-            // write the status immediately
-            display.writeStatus(status);
-        } else {
-            // wait 100 ms for redraw reasons
-            setTimeout(function () {
-                display.writeStatus(status);
-            }, 100);
+        if (this._status) {
+            // refresh to hide the old status message
+            this._status = "";
+            this.refresh();
         }
+        this._status = status;
+        this._display.writeStatus(status);
     }, this);
 
     // used by validators

scripts/player.js
@@ -130,18 +130,21 @@ function Player(x, y, __map, __game) {
             return false;
         }
 
-        if (__map._overrideKeys[direction] && fromKeyboard) {
-            try {
-                __game.validateCallback(__map._overrideKeys[direction], true);
+        if (fromKeyboard) {
+            // clear any status text
+            __map._status = "";
+            if (__map._overrideKeys[direction]) {
+                try {
+                    __game.validateCallback(__map._overrideKeys[direction], true);
 
-                __map.refresh();
-                this._canMove = false;
-                __map._reenableMovementForPlayer(this); // (key delay can vary by map)
-                this._afterMove(__x, __y);
-            } catch (e) {
+                    __map.refresh();
+                    this._canMove = false;
+                    __map._reenableMovementForPlayer(this); // (key delay can vary by map)
+                    this._afterMove(__x, __y);
+                } catch (e) {
+                }
+                return;
             }
-
-            return;
         }
 
         var new__x;